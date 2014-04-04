d041375a5962eb1791f15346b3347353b9fb72ae
Billy Tallis <wtallis@gmail.com>
2014-04-04 19:40:45 -0400

    convert EmptyMod to ForgeGradle format

--- EmptyMod/src/main/java/org/t244/minecraft/EmptyMod.java
+++ EmptyMod/src/main/java/org/t244/minecraft/EmptyMod.java
@@ -1,10 +1,8 @@
 package org.t244.minecraft;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = "T244_EmptyMod", name = "Empty Mod", version = "1.0")
-@NetworkMod(clientSideRequired = true, serverSideRequired = true)
 public class EmptyMod {
 	
 }
