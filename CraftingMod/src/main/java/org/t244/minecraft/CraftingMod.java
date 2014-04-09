2c04c64affbc289bb1233bd973182b9df2d89e3f
William Tallis <wtallis@gmail.com>
2014-04-09 19:12:24 -0400

    convert CraftingMod to ForgeGradle

--- CraftingMod/src/main/java/org/t244/minecraft/CraftingMod.java
+++ CraftingMod/src/main/java/org/t244/minecraft/CraftingMod.java
@@ -1,29 +1,28 @@
 package org.t244.minecraft;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = CraftingMod.modid, name = "CraftingMod", version = "1.0")
-@NetworkMod(clientSideRequired=true, serverSideRequired=true)
 public class CraftingMod {
 	public static final String modid = "T244_CraftingMod";
 
-	@Init
+	@EventHandler
 	public void load(FMLInitializationEvent e) {
 
 		// add a recipe with a shape
-		GameRegistry.addRecipe(new ItemStack(Item.bucketEmpty),
+		GameRegistry.addRecipe(new ItemStack(Items.bucket),
 			new Object[]{
 				// the first two lines give the shape
 				" I ",
 				"I I",
 				// now we explain what item/block corresponds to what letter above
-				'I', Item.ingotIron // we could use an ItemStack to require more than one ingot
+				'I', Items.iron_ingot // we could use an ItemStack to require more than one ingot
 			}
 		);
 	}
