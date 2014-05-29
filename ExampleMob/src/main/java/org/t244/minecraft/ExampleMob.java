93dc4e600d01e2c68a5493a2ce5dbf7f3c059731
William Tallis <wtallis@gmail.com>
2014-05-29 15:32:49 -0400

    ExampleMob: giants no longer burn in sunlight, now attack with severe knockback
    targeting doesn't seem to work 100%: I can get them to attack players and villagers, but not iron golems or each other. They retailate against players but not iron golems.

--- ExampleMob/src/main/java/org/t244/minecraft/ExampleMob.java
+++ ExampleMob/src/main/java/org/t244/minecraft/ExampleMob.java
@@ -1,8 +1,15 @@
 package org.t244.minecraft;
 
-import net.minecraft.entity.ai.EntityAIFleeSun;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.EntityGiantZombie;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.monster.EntityIronGolem;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
@@ -13,7 +20,29 @@ public class ExampleMob extends EntityGiantZombie {
 	public ExampleMob(World w) {
 		super(w);
 
-		tasks.addTask(0,new EntityAIFleeSun(this,5.0));
+		//tasks.addTask(0,new EntityAIFleeSun(this,5.0));
+		this.getNavigator().setBreakDoors(true);
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
+		this.tasks.addTask(4, new EntityAIAttackOnCollide(this, EntityVillager.class, 1.0D, true));
+		this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
+		this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
+		this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.tasks.addTask(8, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+		//this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+		//this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 0, false));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityIronGolem.class, 0, false));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, ExampleMob.class, 0, false));
+	}
+
+	@Override
+	protected void applyEntityAttributes()
+	{
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(50.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(5.0D);
 	}
 
 	@Override
@@ -27,40 +56,49 @@ public class ExampleMob extends EntityGiantZombie {
 	}
 
 	@Override
-	public void onLivingUpdate()
+	protected String getLivingSound() {
+		return "creeper.primed";
+	}
+
+	@Override
+	public boolean attackEntityAsMob(Entity par1Entity)
 	{
-		if (this.worldObj.isDaytime() && !this.worldObj.isRemote && !this.isChild())
+		float damage = (float)this.getEntityAttribute(SharedMonsterAttributes.attackDamage).getAttributeValue();
+		int knockback = 20; // base knockback level
+
+		this.playSound("mob.ghast.moan", 1.0F, 0.125F);
+		if (par1Entity instanceof EntityLivingBase)
 		{
-			float f = this.getBrightness(1.0F);
-
-			if (f > 0.5F && this.rand.nextFloat() * 30.0F < (f - 0.4F) * 2.0F && this.worldObj.canBlockSeeTheSky(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ)))
-			{
-				boolean flag = true;
-				ItemStack itemstack = this.getEquipmentInSlot(4);
-
-				if (itemstack != null)
-				{
-					if (itemstack.isItemStackDamageable())
-					{
-						itemstack.setItemDamage(itemstack.getItemDamageForDisplay() + this.rand.nextInt(2));
-
-						if (itemstack.getItemDamageForDisplay() >= itemstack.getMaxDamage())
-						{
-							this.renderBrokenItemStack(itemstack);
-							this.setCurrentItemOrArmor(4, (ItemStack)null);
-						}
-					}
-
-					flag = false;
-				}
-
-				if (flag)
-				{
-					this.setFire(8);
-				}
-			}
+			damage += EnchantmentHelper.getEnchantmentModifierLiving(this, (EntityLivingBase) par1Entity);
+			knockback += EnchantmentHelper.getKnockbackModifier(this, (EntityLivingBase)par1Entity);
 		}
 
-		super.onLivingUpdate();
+		boolean flag = par1Entity.attackEntityFrom(DamageSource.causeMobDamage(this), damage);
+
+		if (flag)
+		{
+			if (knockback > 0)
+			{
+				par1Entity.addVelocity((double)(-MathHelper.sin(this.rotationYaw * (float) Math.PI / 180.0F) * (float)knockback * 0.5F), 0.1D, (double)(MathHelper.cos(this.rotationYaw * (float)Math.PI / 180.0F) * (float)knockback * 0.5F));
+				this.motionX *= 0.6D;
+				this.motionZ *= 0.6D;
+			}
+
+			int j = EnchantmentHelper.getFireAspectModifier(this);
+
+			if (j > 0)
+			{
+				par1Entity.setFire(j * 4);
+			}
+
+			if (par1Entity instanceof EntityLivingBase)
+			{
+				EnchantmentHelper.func_151384_a((EntityLivingBase)par1Entity, this);
+			}
+
+			EnchantmentHelper.func_151385_b(this, par1Entity);
+		}
+
+		return flag;
 	}
 }
