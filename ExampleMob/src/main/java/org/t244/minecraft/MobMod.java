613d5ceb911355642929a8720c3c998eb0b88ad3
William Tallis <wtallis@gmail.com>
2014-06-10 19:07:07 -0400

    ExampleMob: spawn in extreme hills biomes

--- ExampleMob/src/main/java/org/t244/minecraft/MobMod.java
+++ ExampleMob/src/main/java/org/t244/minecraft/MobMod.java
@@ -2,7 +2,10 @@ package org.t244.minecraft;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
 import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
 
 /**
  * Created by wtallis on 5/14/14.
@@ -13,5 +16,8 @@ public class MobMod {
 	@Mod.EventHandler
 	public void load(FMLInitializationEvent event) {
 		EntityList.addMapping(ExampleMob.class,"examplemob",101, 0xFF00FF, 0x00FF00);
+
+		EntityRegistry.addSpawn(ExampleMob.class, 1, 1, 1, EnumCreatureType.monster,
+				BiomeGenBase.extremeHills, BiomeGenBase.extremeHillsPlus);
 	}
 }
